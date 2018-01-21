569bea3f27dda373b8ecc59bcc24e2ce7007c3e9
Anthony John Ripa <q@t.w>
2018-01-21 12:44:27 -0500

    Newton now recognizes polynomials up to degree 2 with up to 2 variables.

---
+++ README.md
@@ -0,0 +1,20 @@
+
+Newton: An A.I. for Math
+========================
+
+Author : Anthony John Ripa
+Date : 2018.01.21
+
+Newton
+------
+
+Consider the problem (x^2-1)/(x-1). One could attempt to factor the numerator (x+1)(x-1)/(x-1) then divide like terms and get x+1. Alternatively, one could attempt synthetic division and get the same result. These approaches were created by an earlier phase called the learning phase. However, what if we were in a phase before the learning phase where the algorithms did not yet exist. This would be akin to the situation Newton was in before he invented calculus. Well, we could do the two phase approach where we first have a learning phase to make an algorithm to solve the problem, then in the second phase solve the problem using the algorithm from the first phase. This would be more or less like what Newton did: Invent calculus then use calculus. There are other learning approaches, for example lazy learning. In lazy learning, we solve a specific problem instance on the fly (without a previous learning step). We could contrast this with the two phase approach (which for comparison we could call eager learning) but the lines are often blurred. Isaac Newton for example plausibly employed lazy learning specific problem instances on his way to inventing the full calculus. This is the spirit of the software `Newton`. `Newton` uses lazy learning to solve mathematical problems without relying on previously generated algorithms. To solve (x^2-1)/(x-1) `Newton` plugs in a few values and then uses regression to infer the solution is x+1.
+
+Consider the problem ((2+h)^2-2^2)/h | 0. These are secants lines of the function x^2 where the secant intersects x^2 at x=2 and x=2+h . The |0 means evaluate at h=0. This would make the secant line a tangent line. Now a naive way to do this problem is to immediately apply |0 to the number and denominator: ((2+0)^2-2^2)/0 = (2^2-2^2)/0 = (4-4)/0 = 0/0 . Not helpful. We could try to simplify the function ((2+h)^2-2^2)/h = (4+4h+h^2-4)/h = (4h+h^2)/h = 4+h. We could then |0 and get a final answer of 4, which is the right answer. The step (4h+h^2)/h = 4+h has historically been considered problematic. The canonical answer is the piecewise function (h==0 ? undefined : 4+h). This is a line with a hole in it (at exactly the wrong place). If we then apply |0 we get undefined instead of 4. Hence the need for all the rigamarole of calculus. If however, we use Software `Newton`'s lazy learning approach we start with ((2+h)^2-2^2)/h plug in a few values and then use regression to infer the solution is 4+h. It does not conclude that the solution is (h==0 ? undefined : 4+h). Like most machine learning algorithms we get a solution that is smooth (low parameters & complexity), and hence more likely to be correct by Occam's razor. Then apply |0 and get the desired solution 4.
+
+`Newton` solves algebraic problems without reliance on previously generated algorithms. `Newton` does not make the classical algorithm mistake of concluding ((2+h)^2-2^2)/h is (h==0 ? undefined : 4+h) and instead returns the smooth solution 4+h. `Newton`'s approach to algebra is clean enough to answer questions which would otherwise require a needlessly complex workaround like calculus.
+
+
+Dependencies
+------------
+<a href='http://jquery.com'>jQuery</a> and <a href='http://mathjs.org'>mathjs</a>
--- newton.js
+++ newton.js
@@ -1,49 +1,225 @@
-﻿/*
+﻿
+/*
     Author: Anthony John Ripa
-    Date:   12/31/2017
+    Date:   1/21/2018
     Newton: An A.I. for Math
 */
 
 class Newton {
     static simplify(input) {
-        return Newton.ab2poly(Newton.projects(input));
-    }
-    static ab2poly(ab) {
-        var a = round(ab[0])
-        var b = round(ab[1])
-        var c = round(ab[2])
-        return a + '*x ' + (b == 0 ? '' : b == 1 ? ' + h ' : ' + ' + b + '*h ') + (c == 0 ? '' : ' + ' + c);
-        function round(x) {
-            return Math.round(x * 100) / 100;
+        var expr, constant;
+        [expr, constant] = input.split('|');
+        expr = infer(expr)
+        if (constant) {
+            expr = evaluate(expr, constant);
+            expr = infer(expr)
         }
-    }
-    static projects(input) {
-        var x1s = Array(4).fill(0).map(Math.random);
-        var x2s = Array(4).fill(0).map(Math.random);
-        var ys = [];
-        for (var i = 0; i < x1s.length; i++)
-            ys.push(math.eval(Newton.projecth(Newton.projectx(input, x1s[i]), x2s[i])));
-        var b = math.transpose(math.matrix([ys]));
-        var ones = Array(x1s.length).fill(1);
-        var AT = [x1s, x2s, ones];
-        AT = math.matrix(AT);
-        var A = math.transpose(AT);
-        var ab = Newton.solve(A, b);
-        return ab;
-    }
-    static solve(A, b) {
-        var AT = math.transpose(A);
-        var ATA = math.multiply(AT, A);
-        var ATA = math.multiply(AT, A);
-        var ATAinv = math.divide(math.eye(A.valueOf()[0].length), ATA);
-        var ATb = math.multiply(AT, b);
-        var x = math.multiply(ATAinv, ATb);
-        return x.valueOf();
-    }
-    static projectx(input, x) {
-        return input.replace(/x/g, x);
-    }
-    static projecth(input, h) {
-        return input.replace(/h/g, h);
+        return expr;
+        function evaluate(input, val) {
+            return substitute(input, getvars(input).slice(-1), val);
+        }
+        function substitute(input, vari, val) {
+            return input.replace(new RegExp(vari, 'g'), '(' + val + ')');
+        }
+        function getvars(input) {
+            var vars = [];
+            var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+            for (var i = 0; i < input.length; i++) {
+                var symbol = input[i];
+                if (isin(symbol, alphabet)) {
+                    if (!isin(symbol, vars)) vars.push(symbol);
+                }
+            }
+            return vars;
+            function isin(needle, haystack) {
+                return haystack.indexOf(needle)>-1;
+            }
+        }
+        function infer(input) {
+            var vars = getvars(input);
+            var input2matrix, toString;
+            [input2matrix, toString] = parser22();
+            var vect = solve(...input2matrix(input));
+            return toString(vect, vars);
+            function makexs(vars) {
+                var xs = [];
+                xs.ones = Array(10).fill(1);
+                for (var i = 0; i < vars.length; i++) xs.push(xs.ones.map(Math.random));
+                return xs;
+            }
+            function solve(A, b) {
+                var AT = math.transpose(A);
+                var ATA = math.multiply(AT, A);
+                var ATA = math.multiply(AT, A);
+                var ATAinv = math.divide(math.eye(A[0].length), ATA);
+                var ATb = math.multiply(AT, b);
+                var x = math.multiply(ATAinv, ATb);
+                return x.valueOf();
+            }
+            function round(x) {
+                return Math.round(x * 100) / 100;
+            }
+            function coef(num, vari) {
+                if (num == 0) return '';
+                if (num == 1) return '+' + vari;
+                return '+' + num + (vari == '1' ? '' : vari);
+            }
+            function parser22() {
+                return [
+                    function input2matrix22(input) {
+                        var vars = getvars(input);
+                        var xs = makexs(vars);
+                        var A = makeA(xs);
+                        var b = makeb(xs, input);
+                        return [A, b];
+                        function makeA(xs) {
+                            var AT = [];
+                            if (xs.length == 0) {
+                                AT.push(xs.ones);
+                            } else if (xs.length == 1) {
+                                AT = AT.concat(makepowercols(xs[0]));
+                            } else if (xs.length == 2) {
+                                AT = AT.concat(makepowercols(xs[0]));
+                                AT = AT.concat(makepowercols(xs[0]).map(x=>coltimescol(x, xs[1])));
+                                AT = AT.concat(makepowercols(xs[0]).map(x=>coltimescol(x, xs[1].map(xi=>math.pow(xi, 2)))));
+                            }
+                            return math.transpose(AT);
+                            function coltimescol(col1, col2) {
+                                var ret = [];
+                                for (var i = 0; i < col1.length; i++)
+                                    ret.push(col1[i] * col2[i]);
+                                return ret;
+                            }
+                            function makepowercols(x) {
+                                var cols = [];
+                                for (var power = 0; power <= 2; power++) cols.push(x.map(xi=>math.pow(xi, power)));
+                                return cols;
+                            }
+                        }
+                        function makeb(xs, input) {
+                            var ys = [];
+                            for (var datai = 0; datai < xs.ones.length; datai++) {
+                                var expression = input;
+                                for (var varj = 0; varj < vars.length; varj++) {
+                                    expression = substitute(expression, vars[varj], xs[varj][datai]);
+                                }
+                                ys.push(math.eval(expression));
+                            }
+                            var b = math.transpose([ys]);
+                            return b;
+                        }
+                    },
+                    function toString22(vect, vars) {
+                        //console.log(vect.join('\n'))
+                        //console.log(vars)
+                        //console.log(vars.length)
+                        var varcombo = [];
+                        if (vars.length == 0) varcombo.push('1');
+                        if (vars.length == 1) {
+                            for (var v1 = 0; v1 <= 2; v1++)
+                                varcombo.push(term(v1, '1', vars));
+                        }
+                        if (vars.length == 2) {
+                            for (var v2 = 0; v2 <= 2; v2++)
+                                for (var v1 = 0; v1 <= 2; v1++)
+                                    varcombo.push(term(v1, v2, vars));
+                        }
+                        vect = vect.map(round);
+                        var ret = '';
+                        for (var i = 0; i < varcombo.length; i++) {
+                            ret += i == 0 ? vect[i] : coef(vect[i], varcombo[i]);
+                        }
+                        if (ret.length == 1) return ret;
+                        if (ret[0] == '0') ret = ret.substr(1);
+                        if (ret[0] == '+') ret = ret.substr(1);
+                        return ret;
+                        function term(i, j, vars) {
+                            if (vars.length == 1) return subterm(vars[0], i);
+                            if (j == 0) return subterm(vars[0], i);
+                            if (i == 0) return subterm(vars[1], j);
+                            return subterm(vars[0], i) + '*' + subterm(vars[1], j);
+                            function subterm(vari, ind) {
+                                if (ind == 0) return '';
+                                return vari + (ind == 1 ? '' : '^' + ind);
+                            }
+                        }
+                    }
+                ];
+            }
+            function parser2() {
+                return [
+                    function input2matrix2(input) {
+                        var vars = getvars(input);
+                        var xs = makexs(vars);
+                        var A = makeA(xs);
+                        var b = makeb(xs, input);
+                        return [A, b];
+                        function makeA(xs) {
+                            var ones = Array(xs[0].length).fill(1);
+                            var AT = [ones].concat(xs);
+                            var A = math.transpose(AT);
+                            return A;
+                        }
+                        function makeb(xs, input) {
+                            var ys = [];
+                            for (var datai = 0; datai < xs[0].length; datai++) {
+                                var expression = input;
+                                for (var varj = 0; varj < vars.length; varj++) {
+                                    expression = substitute(expression, vars[varj], xs[varj][datai]);
+                                }
+                                ys.push(math.eval(expression));
+                            }
+                            var b = math.transpose([ys]);
+                            return b;
+                        }
+                    },
+                    function toString2(vect, vars) {
+                        vect = vect.map(round);
+                        var ret = '';
+                        console.log(vars)
+                        console.log(vect)
+                        for (var i = 0; i < vars.length; i++) {
+                            ret += coef(vect[i + 1], vars[i]);
+                        }
+                        if (vect[0] != 0) ret += '+' + vect[0];
+                        return ret.substr(1);
+                    }
+                ];
+            }
+            function parser1() {
+                return [
+                    function input2matrix1(input) {
+                        var vars = getvars(input);
+                        var xs = makexs(vars);
+                        var A = makeA(xs);
+                        var b = makeb(xs, input);
+                        return [A, b];
+                        function makeA(xs) {
+                            var AT = [];
+                            for (var power = 0; power <= 2; power++) AT.push(xs[0].map(xi=>math.pow(xi, power)));
+                            return math.transpose(AT);
+                        }
+                        function makeb(xs, input) {
+                            var ys = [];
+                            for (var datai = 0; datai < xs[0].length; datai++) {
+                                var expression = input;
+                                expression = substitute(expression, vars[0], xs[0][datai]);
+                                ys.push(math.eval(expression));
+                            }
+                            var b = math.transpose([ys]);
+                            return b;
+                        }
+                    },
+                    function toString1(vect, vars) {
+                        vect = vect.map(round);
+                        var ret = '';
+                        for (var i = 0; i < vect.length; i++) {
+                            ret += coef(vect[i], vars[0] + '^' + i);
+                        }
+                        return ret.substr(1);
+                    }
+                ];
+            }
+        }
     }
 }
