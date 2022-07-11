230ef757a37a83637e59c96f7100013adfd4c7d1
Anthony John Ripa <you@example.com>
2022-07-10 22:45:10 -0400

    Newton now recognizes E-notation.

--- expression.js
+++ expression.js
@@ -1,24 +1,39 @@
 
 /*
 	Author:	Anthony John Ripa
-	Date:	05/10/2022
+	Date:	7/10/2022
 	Newton:	An A.I. for Math
 */
 
-
 class Expression {
 
-	static getvars(input) {
-		input = input.toString();	//	+2021.11
-		input = input.replaceAll('sinh','').replaceAll('sin','').replaceAll('cosh','').replaceAll('cos','').replaceAll('exp','');
-		var vars = [];
-		var alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		for (let symbol of input) {
-			if (alphabet.includes(symbol)) {
-				if (!vars.includes(symbol)) vars.push(symbol);
+	//static getvars(input) {		//	-2022.7
+	//	input = input.toString();	//	+2021.11
+	//	input = input.replaceAll('sinh','').replaceAll('sin','').replaceAll('cosh','').replaceAll('cos','').replaceAll('exp','');
+	//	var vars = [];
+	//	var alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	//	for (let symbol of input) {
+	//		if (alphabet.includes(symbol)) {
+	//			if (!vars.includes(symbol)) vars.push(symbol);
+	//		}
+	//	}
+	//	return vars;
+	//}
+
+	static getvars(str) {			//	+2022.7
+		let bag = getvarsnode(math.parse(str));
+		let set = [...new Set(bag)];
+		return set;
+		function getvarsnode(node) {
+			switch (node.type) {
+				case 'ConstantNode': return [];
+				case 'SymbolNode': return [node.name];
+				case 'ParenthesisNode': return getvarsnode(node.content);
+				case 'OperatorNode':
+				case 'FunctionNode': return node.args.map(getvarsnode).flat();
+				default: alert('Expression.getvars Error : ' + node.type)
 			}
 		}
-		return vars;
 	}
 
 	static evaluate(input, val) {
@@ -27,7 +42,6 @@ class Expression {
 
 	static substitute(input, vari, val) {
 		if (vari === undefined) return input;
-		//var scope = JSON.parse('{' + '"' + vari + '"' + ':' + val + '}');	//	-2022.05
 		var scope = {};														//	+2022.05
 		scope[vari] = Number(val);											//	+2022.05
 		try {												//	+2021.11
